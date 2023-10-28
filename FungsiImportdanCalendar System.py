15a373374f75d2d2f18efc69b1ff6682bfea3c17
Zach Noland <muzackyulum@gmail.com>
2023-10-28 16:44:57 +0700

    first commit

---
+++ FungsiImportdanCalendar System.py
@@ -0,0 +1,9 @@
+import calendar
+yy = 2023 # year
+mm = 10 #month
+
+#display the calendar
+
+print(calendar.month(yy,mm))
+
+#Fungsi Import Pada Pemrograman Python, pada python import berfungsi untuk multi file maksudnya yaitu kita dapat memanggil file lain di dalam satu module yang berbeda.
