3f581cc44052d935f87cf2581e4a0b2b6d9a93f4
Zach Noland <muzackyulum@gmail.com>
2023-10-29 11:05:55 +0700

    update

--- Python Project/Shortcut Beta.py
+++ Python Project/Shortcut Beta.py
@@ -25,11 +25,12 @@ def main_menu():
     print("2. This PC")
     print("3. Documents")
     print("4. Downloads")
-    print("5. Credits")
-    print("6. Exit")
+    print("5. Wifi Password Beta(Only see wifi history)")
+    print("6. Credits")
+    print("7. Exit")
 
 def prompt_cmd():
-    subprocess.call('start cmd', shell=True)
+    subprocess.call('start cmd /k "dir"', shell=True)
 
 def open_thispc():
     subprocess.Popen('explorer /select,shell:ThisPCFolder', shell=True)
@@ -39,6 +40,20 @@ def show_documents():
 
 def show_downloads():
     subprocess.Popen('explorer shell:Downloads', shell=True)
+
+def show_password():
+    command = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
+    profiles = [i.split(":")[1][1:-1] for i in command if "All User Profile" in i]
+    for i in profiles:
+        results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split('\n')
+        results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+        try:
+            print ("{:<30}|  {:<}".format(i, results[0]))
+        except IndexError:
+            print ("{:<30}|  {:<}".format(i, ""))
+    
+    input("Tekan Enter untuk kembali ke menu...")
+
 
 def show_credits():
     print("")
@@ -58,8 +73,10 @@ while True:
     elif selection == "4":
         show_downloads()
     elif selection == "5":
-        show_credits()
+        show_password()
     elif selection == "6":
+        show_credits()
+    elif selection == "7":
         print("Terima kasih karena telah mencoba beta ini. Program selesai.")
         break
     else:
